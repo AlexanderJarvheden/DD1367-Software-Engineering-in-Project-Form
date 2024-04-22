602971bf9e8525851045434750310e1b4f8ef54b
Joachim Olsson <joaco@kth.se>
2024-04-22 19:17:01 +0200

    fix small position stuff

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,13 @@ import Landingpage from './Components/Landingpage.js';
 import SignupPage from './Components/SignupPage';
 import LoginPage from './Components/LoginPage.js';
 import Navigationbar from './Components/Navigationbar.js';
+import ButtonMeny from './Components/ButtonMeny.js';
 
 
 function App() {
   return (
     <div className="App">
-      <Navigationbar/>
-      <SignupPage/>
+      <ButtonMeny />
     </div>
 
   );
---
+++ client/src/Components/ButtonMeny.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import '../Assets/Styles/buttonmeny.css';
+import menu_logo from '../Assets/Icons/menu_logo.svg'
+import dashboard_icon from '../Assets/Icons/dashboard_icon.svg'
+import library_icon from '../Assets/Icons/library_icon.svg'
+import social_icon from '../Assets/Icons/social_icon.svg'
+import teams_icon from '../Assets/Icons/teams_icon.svg'
+import projects_icon from '../Assets/Icons/projects_icon.svg'
+import account_icon from '../Assets/Icons/account_icon.svg'
+
+
+function Sidebar() {
+    const [activeItem, setActiveItem] = useState('dashboard');
+
+    const menuItems = [
+        { name: 'dashboard', text: 'Dashboard', icon: dashboard_icon },
+        { name: 'library', text: 'Library', icon: library_icon },
+        { name: 'planner', text: 'Planner', icon: library_icon },
+        { name: 'social', text: 'Social', icon: social_icon },
+        { name: 'teams', text: 'Teams', icon: teams_icon },
+        { name: 'projects', text: 'Projects', icon: projects_icon },
+        { name: 'account', text: 'Account', icon: account_icon },
+
+    ];
+
+    return (
+        <div className="sidebar">
+            <div className="logo">
+                <img src={menu_logo} alt="Menu Logo" />
+            </div>
+
+            {menuItems.map(item => (
+                <div
+                    key={item.name}
+                    className={`menu-item ${activeItem === item.name ? 'active' : ''}`}
+                    onClick={() => setActiveItem(item.name)}
+                >
+                    <div className="menu-icon">
+                        {<img src={item.icon} alt={`${item.text} icon`} />}
+                    </div>
+                    <div className="menu-text">{item.text}</div>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default Sidebar;
