4fe61ebcc1a806b391d895009260bb5a3c8b7c84
Tim Bergbom <tbergbom@kth.se>
2024-04-24 16:19:13 +0200

    add all to main

---
+++ database_management/connection.js
@@ -0,0 +1,28 @@
+
+import { Sequelize } from 'sequelize'
+
+const sequelize = new Sequelize({
+    database: "loopbox",
+    username: "falu",
+    password: "Xcz9oGvg",
+    host: "psql-dd1368-ht23.sys.kth.se",
+    port: 5432,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true, // This will help you. But you will see nwe error
+        rejectUnauthorized: false // This line will fix new error
+      }
+    },
+});
+
+async function connectToDb() {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
+}
+
+connectToDb();
