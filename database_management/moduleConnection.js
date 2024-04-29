dac2b9b17341d504a0e8e490ef87371397752c1f
klov <klov@kth.se>
2024-04-29 03:18:29 +0200

    lagt till login metod som behöver kompleteras

--- database_management/moduleConnection.js
+++ database_management/moduleConnection.js
@@ -1,4 +1,5 @@
 import { Sequelize, DataTypes } from 'sequelize';
+//import bcrypt from 'bcrypt';
 
 // fixa Sequelize connection
 const sequelize = new Sequelize({
@@ -74,12 +75,11 @@ async function testAddUser() {
 testAddUser();
 
 /**
- * Method to add user to database
- * @param {Object} userData 
+ * Method to add user to database. It currently does not work.
+ * @param {Object} userData innehåller name, email, password, tlfnr och company
  * @returns logged message for either success of failure to add user
  */
 async function addUser(userData) {
-
     const { name, email, password, phonenumber, company } = userData;
 
     // Där har ni alla variabler med fyllda värden som användare använt
@@ -87,14 +87,51 @@ async function addUser(userData) {
     // Notera att vi inte har UserAgreement. Vi kommer inte skapa ett konto om den rutan inte är ifylld.
     
     try {
-        const newUser = await User.create(placeholder);
-        console.log('User added successfully:', newUser.toJSON());
-        return newUser; // retunera det skapade användar objektet
+        // Kolla först att email inte redan finns, kolla alla andra saker också?
+        const user = await User.findOne({ where: { email: email } });
+        if (user) {
+            console.log('Signup failed: Email already in use');
+            throw new Error('Email already in use')
+        } else {
+            const newUser = await User.create(userData);
+            console.log('User added successfully:', newUser.toJSON());
+            return newUser; // retunera det skapade användar objektet
+        }
     } catch (error) {
         console.error('Error adding user:', error);
         throw error; // kasta error om fel
     }
 }
 
+/**
+ * Method to log in user. Currently does not work. It does not check anything in the database.
+ * @param {Object} userData innehåller email och password
+ * @returns 
+ */
+async function login(userData) {
+    const { email, password } = req.body;
+    
+    try {
+        const user = await User.findOne({ where: { email: email } });
+        if (!user) {
+            console.log('Authentication failed: No user found with that email');
+            return { error: 'Authentication failed: Invalid email or password' }; 
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (isMatch) {
+            console.log('Authentication successful:', user.toJSON());
+            return { user: user.toJSON() }; 
+        } else {
+            console.log('Authentication failed: Password incorrect');
+            return { error: 'Authentication failed: Invalid email or password' }; 
+        }
+    } catch (error) {
+        console.error('Error searching for user:', error);
+        throw error;
+    }
+}
+
 // För att vi ska kunna kalla på den genom POST metoden i servers index.js
 export { addUser };
+export { login };
