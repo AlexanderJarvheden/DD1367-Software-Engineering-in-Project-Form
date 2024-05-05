1112a063e60d2e20bbe1e57969b54d22faebcd97
AlexanderJarvheden <alexander.jarvheden@gmail.com>
2024-05-05 18:20:11 +0200

    Make static worksheet for 'product journey mapping' tool

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,10 @@
 import './App.css';
-import './Assets/Styles/landingpage.css'
-import Landingpage from './Components/Landingpage.js';
-import SignupPage from './Components/SignupPage';
-import LoginPage from './Components/LoginPage.js';
-import Navigationbar from './Components/Navigationbar.js';
-import PinLibraryOfTools from './Components/PinLibraryOfTools.js';
-import ContactPage from './Components/ContactPage.js';
-import PostSignUpPage from './Components/PostSignUpPage.js';
-import ChooseTeam from './Components/ChooseTeam.js'
+import PJMWorksheet from './Tools/PJMWorksheet.js'
 
 function App() {
   return (
     <div className="App">
-      <Navigationbar/>
-      <ChooseTeam/>
+      <PJMWorksheet/>
     </div>
 
   );
---
+++ client/src/Components/ToolNavBar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import '../Assets/Styles/toolNavBar.css'
+import openBook from '../Assets/Icons/openBook.svg'
+import reduceScreen from '../Assets/Icons/reduceScreen.svg'
+import fullscreen from '../Assets/Icons/fullscreen.svg'
+
+
+const ToolNavBar = (props) => {
+  return (
+    <div style={props.style}>
+
+      <div className='container'>
+        <button className='button'>
+          <img src={openBook} alt='' style={{
+            width: '24px',
+            height: '24px'
+          }} />
+          <span className='headerText'>Instructions</span>
+        </button>
+        <button className='button'>
+          <img src={reduceScreen} alt='' style={{
+            width: '24px',
+            height: '24px'
+          }} />
+          <span className='headerText'>Condense planner</span>
+        </button>
+        <button className='button'>
+          <img src={fullscreen} alt='' />
+          <span className='headerText'>Enlarge planner</span>
+        </button>
+      </div>
+    </div>
+  )
+}
+export default ToolNavBar
