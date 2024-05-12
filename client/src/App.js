c548b355e3598ee45f14dda76238f965c7df3f5c
klov <klov@kth.se>
2024-05-12 23:10:56 +0200

    Added imports to all current pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,15 @@
-import {Route, Routes } from "react-router-dom"
+import {Link, Route, Routes } from "react-router-dom"
 import './App.css';
+import './Assets/Styles/landingpage.css'
 import PJMWorksheet from './Tools/PJMWorksheet.js'
+import Landingpage from './Components/Landingpage.js';
+import SignupPage from './Components/SignupPage';
+import LoginPage from './Components/LoginPage.js';
+import Navigationbar from './Components/Navigationbar.js';
+import PinLibraryOfTools from './Components/PinLibraryOfTools.js';
+import ContactPage from './Components/ContactPage.js';
+import PostSignUpPage from './Components/PostSignUpPage.js';
+
 
 function App() {
   return (
