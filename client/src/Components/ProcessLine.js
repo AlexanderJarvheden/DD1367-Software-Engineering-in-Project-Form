c040ef97b48253ecb6da6e21f81a683a956f76e9
zizo2001 <aziz.alfta@gmail.com>
2024-05-31 12:00:46 +0200

    the planner is done

--- client/src/Components/ProcessLine.js
+++ client/src/Components/ProcessLine.js
@@ -1,15 +1,13 @@
 import React, { useState } from 'react';
 import '../Assets/Styles/ProcessLine.css';
 import PinLibraryOfTools from './PinLibraryOfTools';
-import Buttons from '../Pages/DashboardPage.js'
-
 
 const ProcessLine = () => {
   const [circles, setCircles] = useState([]);
   const [showPinLibrary, setShowPinLibrary] = useState(false);
 
   const handleAddCircle = () => {
-    if (circles.length < 10) {
+    if (circles.length < 30) { // Updated to handle up to 30 circles
       setShowPinLibrary(true);
     }
   };
@@ -24,24 +22,23 @@ const ProcessLine = () => {
   };
 
   return (
-    <div>
-      <Buttons />
-      <div className="process-line-container">
-        <div className="process-line-header">
-          <div className="process-line-text-wrapper">
-            <span className="process-line-text">START</span>
-          </div>
-          <div className="process-line-text-wrapper">
-            <span className="process-line-text">END</span>
-          </div>
+    <div className="process-line-container">
+      <div className="process-line-header">
+        <div className="process-line-text-wrapper">
+          <span className="process-line-text">START</span>
         </div>
+        <div className="process-line-text-wrapper">
+          <span className="process-line-text">END</span>
+        </div>
+      </div>
+      <div className="process-line-wrapper">
         <div className="process-line">
-          {circles.map((circle, index) => (
-            <div key={index} className="addCircle">
+          {circles.slice(0, 10).map((circle, index) => (
+            <div key={index} className="process-circle">
               {circle.tool ? (
                 circle.tool
               ) : (
-                <div className="purple-circle">
+                <div className="process-circle-placeholder">
                   <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 45 45">
                     <circle cx="22.5" cy="22.5" r="21.5" />
                   </svg>
@@ -54,51 +51,110 @@ const ProcessLine = () => {
               <div className="process-line-add">
                 <span className="add-icon">+</span>
               </div>
-              <span className="add-text">ADD NEW</span>
+              <div className="add-text-wrapper">
+                <span className="add-text">ADD NEW</span>
+              </div>
             </div>
           )}
-          {circles.length === 10 && (
-            <>
-              <div className="curved-section"></div>
-              <div className="straight-section"></div>
-            </>
-          )}
         </div>
-
-        <div className="circle-container">
-          <div className="addCircle purple-circle">
-            <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 45 45">
-              <circle cx="22.5" cy="22.5" r="21.5" />
-            </svg>
-            <span className="circle-text">ACTIVITY DONE</span>
-          </div>
-          <div className="addCircle red-circle">
-            <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 45 45">
-              <circle cx="22.5" cy="22.5" r="21.5" />
-            </svg>
-            <span className="circle-text">RESEARCH</span>
-          </div>
-          <div className="addCircle yellow-circle">
-            <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 45 45">
-              <circle cx="22.5" cy="22.5" r="21.5" />
-            </svg>
-            <span className="circle-text">IDEATION</span>
-          </div>
-          <div className="addCircle blue-circle">
-            <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 45 45">
-              <circle cx="22.5" cy="22.5" r="21.5" />
-            </svg>
-            <span className="circle-text">EVALUATION</span>
-          </div>
-        </div>
-        <div className="bottom-bar"></div>
-
-        {showPinLibrary && (
-          <div className="overlay">
-            <PinLibraryOfTools onClose={closePinLibrary} onSelectTool={handleSelectTool} />
-          </div>
+        {circles.length >= 10 && (
+          <>
+            <div className="curved-section"></div>
+            <div className="straight-section"></div>
+            <div className="second-curved-section"></div>
+            <div className="second-straight-section">
+              {circles.slice(10, 20).map((circle, index) => (
+                <div key={index} className="process-circle">
+                  {circle.tool ? (
+                    circle.tool
+                  ) : (
+                    <div className="process-circle-placeholder">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 45 45">
+                        <circle cx="22.5" cy="22.5" r="21.5" />
+                      </svg>
+                    </div>
+                  )}
+                </div>
+              ))}
+              {circles.length < 20 && (
+                <div className="process-line-add-wrapper second-add-wrapper" onClick={handleAddCircle}>
+                  <div className="process-line-add">
+                    <span className="add-icon">+</span>
+                  </div>
+                  <div className="add-text-wrapper">
+                    <span className="add-text">ADD NEW</span>
+                  </div>
+                </div>
+              )}
+            </div>
+          </>
+        )}
+        {circles.length >= 20 && (
+          <>
+            <div className="third-curved-section"></div>
+            <div className="third-straight-section"></div>
+            <div className="fourth-curved-section"></div>
+            <div className="fourth-straight-section">
+              {circles.slice(20, 30).map((circle, index) => (
+                <div key={index} className="process-circle">
+                  {circle.tool ? (
+                    circle.tool
+                  ) : (
+                    <div className="process-circle-placeholder">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 45 45">
+                        <circle cx="22.5" cy="22.5" r="21.5" />
+                      </svg>
+                    </div>
+                  )}
+                </div>
+              ))}
+              {circles.length < 30 && (
+                <div className="process-line-add-wrapper third-add-wrapper" onClick={handleAddCircle}>
+                  <div className="process-line-add">
+                    <span className="add-icon">+</span>
+                  </div>
+                  <div className="add-text-wrapper">
+                    <span className="add-text">ADD NEW</span>
+                  </div>
+                </div>
+              )}
+            </div>
+          </>
         )}
       </div>
+      <div className="circle-container">
+        <div className="addCircle purple-circle">
+          <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 45 45">
+            <circle cx="22.5" cy="22.5" r="21.5" />
+          </svg>
+          <span className="circle-text">ACTIVITY DONE</span>
+        </div>
+        <div className="addCircle red-circle">
+          <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 45 45">
+            <circle cx="22.5" cy="22.5" r="21.5" />
+          </svg>
+          <span className="circle-text">RESEARCH</span>
+        </div>
+        <div className="addCircle yellow-circle">
+          <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 45 45">
+            <circle cx="22.5" cy="22.5" r="21.5" />
+          </svg>
+          <span className="circle-text">IDEATION</span>
+        </div>
+        <div className="addCircle blue-circle">
+          <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 45 45">
+            <circle cx="22.5" cy="22.5" r="21.5" />
+          </svg>
+          <span className="circle-text">EVALUATION</span>
+        </div>
+      </div>
+      <div className="bottom-bar"></div>
+
+      {showPinLibrary && (
+        <div className="overlay">
+          <PinLibraryOfTools onClose={closePinLibrary} onSelectTool={handleSelectTool} />
+        </div>
+      )}
     </div>
   );
 };
